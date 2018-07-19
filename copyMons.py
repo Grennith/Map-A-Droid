4093c377d86fc23b06b20e59b0a38fb2d586140c
Christoph Eitelhuber <cec@retrospot.de>
2018-07-19 22:23:06 +0200

    Change Raidmons Regirock & Zapdos

--- copyMons.py
+++ copyMons.py
@@ -28,7 +28,7 @@ class MonRaidImages(object):
                     2: [125, 303, 185, 310],
                     3: [68, 142, 135, 95],
                     4: [359, 248, 76, 112],
-                    5: [378, 249]
+                    5: [377, 145]
                 }
 
         if not os.path.exists(assetPath):
