9ad603d5c427b44ddeac3383bf75e8bfdb7b2df9
Christoph Eitelhuber <cec@retrospot.de>
2018-08-02 07:52:26 +0200

    better crops

--- matching.py
+++ matching.py
@@ -35,9 +35,9 @@ def fort_image_matching(url_img_name, fort_img_name, zoom, value, raidNo, hash, 
             os.remove(tempFile)
         else:
             if height_f > width_f:            
-                fort_img = fort_img[int((height_f/2)-(height_f/4)):int((height_f/2)+(height_f/4)), int(0):int(width_f)]
+                fort_img = fort_img[int((height_f/2)-(height_f/3)):int((height_f/2)+(height_f/3)), int(0):int(width_f)]
             else:
-                fort_img = fort_img[int(0):int(height_f), int((width_f/2)-(width_f/4)):int((width_f/2)+(width_f/4))]
+                fort_img = fort_img[int(0):int(height_f), int((width_f/2)-(width_f/3)):int((width_f/2)+(width_f/3))]
             #fort_img = fort_img
             #cv2.imwrite('Gym_' + str(fort_img_name) + '.png', fort_img)
             
