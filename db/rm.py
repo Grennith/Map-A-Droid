004050a87c7dab3d1bec76f86f727623994409cc
Christoph Eitelhuber <cec@retrospot.de>
2018-09-25 21:57:13 +0200

    fix raid start without egg (only RMbug)

--- db/rm.py
+++ db/rm.py
@@ -319,7 +319,7 @@ class RmWrapper:
         whereStr = 'WHERE gym_id = \'%s\' ' % str(gym)
         if MonWithNoEgg:
             # submit mon without egginfo -> we have an endtime
-            start = end - 45 * 60 * 4
+            start = end - (int(args.raid_time) * 60)
             log.info("Updating mon without egg")
             setStr = 'SET level = %s, spawn = FROM_UNIXTIME(%s), start = FROM_UNIXTIME(%s), end = FROM_UNIXTIME(%s), ' \
                      'pokemon_id = %s, last_scanned = FROM_UNIXTIME(%s), cp = %s, move_1 = %s, move_2 = %s '
