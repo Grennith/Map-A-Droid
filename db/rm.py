b3ca55e197fef946be813d8cf76330c63c738362
Christoph Eitelhuber <cec@retrospot.de>
2018-10-29 18:10:55 +0100

    Fix Timestamp error with RM and Monwithoutegg

--- db/rm.py
+++ db/rm.py
@@ -310,11 +310,11 @@ class RmWrapper:
             return False
 
         if start is not None:
-            #start -= self.timezone * 60 * 60
+            tsstart = start
             start = datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S')
 
         if end is not None:
-            #end -= self.timezone * 60 * 60
+            tsend = end
             end = datetime.datetime.utcfromtimestamp(end).strftime('%Y-%m-%d %H:%M:%S')
 
         wh_send = False
@@ -334,7 +334,7 @@ class RmWrapper:
         if MonWithNoEgg:
             # submit mon without egginfo -> we have an endtime
             #start = end - (int(args.raid_time) * 60)
-            start = datetime.datetime.utcfromtimestamp(int(end) - (int(args.raid_time) * 60)).strftime('%Y-%m-%d %H:%M:%S')
+            start = datetime.datetime.utcfromtimestamp(int(tsend) - (int(args.raid_time) * 60)).strftime('%Y-%m-%d %H:%M:%S')
             log.info("Updating mon without egg")
             setStr = 'SET level = %s, spawn = FROM_UNIXTIME(%s), start = %s, end = %s, ' \
                      'pokemon_id = %s, last_scanned = FROM_UNIXTIME(%s), cp = %s, move_1 = %s, move_2 = %s '
